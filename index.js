aedcc45f841de95853ded38aaf17a79a26f7fa17
Justin Cai <justinxcai@gmail.com>
2019-08-29 23:26:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,6 +23,8 @@ var funkyFunction = function() {
     return "FUNKY!"
   }
 }
+function funkyFunction()
+
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
